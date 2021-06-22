44c3937c232315a4342fc1f9570ed7d6f04cb94e
joshua braclett <joshua.brackett@hotmail.ca>
2021-06-21 20:45:12 -0400

    more heroku deployment tests

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.default_url_options = { host: 'https://still-bayou-01166.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://guarded-woodland-15479.herokuapp.com/' }
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
